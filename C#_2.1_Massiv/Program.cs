2a0ac84432636ee621cea2813a70a3470305c6fa
Aleksey <pilot-1981@yandex.ru>
2022-08-23 15:04:37 +0500

    add Lession_C#_2

---
+++ C#_2.1_Massiv/Program.cs
@@ -0,0 +1,23 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2> result) result = arg2;
+    if (arg3> result) result = arg3;
+    return result;
+}
+//              0  1  2  3  4  5  6  7  8 - элементы массива
+int [] array = {11,21,31,41,51,616,71,81,91}; // массив
+
+int resultMax = Max (
+    Max (array[0], array[1], array[2]),
+    Max (array[3], array[4], array[5]),
+    Max (array[6], array[7], array[8])
+);
+Console.WriteLine(resultMax);
+
+//int max = Max ();
+//    Max(array, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+//
+//Console.WriteLine(max);
---
+++ C#_2.2/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] array = {11,21,31,41,91,616,71,81,91}; // массив
+
+int n = array.Length; // возвращает длину (количество) элементов массива
+int find = 91;
+
+int index = 0;
+
+while (index < n)
+{
+    
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // в случае выполнения условия, прервется выполнение
+    }
+    // index = index + 1;
+    index++;
+}
